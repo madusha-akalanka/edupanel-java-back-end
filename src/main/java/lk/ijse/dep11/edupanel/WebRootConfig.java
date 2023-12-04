a26906cde2ec9c26edc8ebd2a795d850182a067d
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-12-04 13:50:39 +0530

    Setup Routes and Database

---
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep11.edupanel;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+}
